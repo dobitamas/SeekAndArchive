bd2e9be0f7071ee04f3ae89533cfd86312588694
dobitamas <totika10@gmail.com>
2020-07-18 14:27:56 +0200

    Add project files.

---
+++ SeekAndArchive/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeekAndArchive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello! Please provide the name of the file below which you are searching for");
+            FileInfo fileInfo = new FileInfo(Console.ReadLine() ?? throw new InvalidOperationException("The file name can't be blank!"));
+            Console.WriteLine("And now please provide the path of the directory where to search below!");
+            DirectoryInfo directoryInfo = new DirectoryInfo(Console.ReadLine() ?? throw new InvalidOperationException("The path of the directory can't be blank!"));
+        }
+
+        public string[] Search(FileInfo fileInfo, DirectoryInfo directoryInfo)
+        {
+            //TODO
+            throw new NotImplementedException();
+        }
+    }
+}
